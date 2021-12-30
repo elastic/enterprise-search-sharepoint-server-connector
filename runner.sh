cb4ee49bbadcbdea3ee19658aeb98a73209e86cd
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2021-12-30 12:49:52 +0100

    Add Elastic License header to all source code files

--- runner.sh
+++ runner.sh
@@ -1,1 +1,7 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 python3 fullsync.py & python3 fetch_index.py & python3 deindex.py & python3 sync_user_permissions.py
