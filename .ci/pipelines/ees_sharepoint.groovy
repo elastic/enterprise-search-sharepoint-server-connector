78fadf8400309945e3d887930a1d5868add9c931
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-01-26 22:31:40 +0100

    Actual groovy script

--- .ci/pipelines/ees_sharepoint.groovy
+++ .ci/pipelines/ees_sharepoint.groovy
@@ -1,33 +1,28 @@
----
+//
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+//
 
-- job:
-    name: ees_sharepoint/linters
-    description: "Runs flake8 linter against the enterprise-search-sharepoint-server-2016-connector repository"
-    project-type: multibranch
-    node: master
-    concurrent: true
-    script-path: .ci/pipelines/ees_sharepoint.groovy
-    prune-dead-branches: true
-    scm:
-      - github:
-          repo: enterprise-search-sharepoint-server-2016-connector
-          repo-owner: elastic
-          disable-pr-notifications: true
-          branch-discovery: all
-          discover-pr-origin: current
-          discover-pr-forks-strategy: false
-          discover-pr-forks-trust: nobody
-          discover-tags: false
-          build-strategies:
-            - regular-branches: true
-          property-strategies:
-            all-branches:
-              - pipeline-branch-durability-override: performance-optimized
-          credentials-id: 2a9602aa-ab9f-4e52-baf3-b71ca88469c7-UserAndToken
-          ssh-checkout:
-            credentials: f6c7695a-671e-4f4f-a331-acdce44ff9ba
-          clean:
-            after: true
-            before: true
-          prune: true
-          timeout: 10
+// Loading the shared lib
+@Library(['estc', 'entsearch']) _
+
+eshPipeline(
+    timeout: 45,
+    project_name: 'Enterprise Search Sharepoint Server 2016 Connector',
+    repository: 'enterprise-search-sharepoint-server-2016-connector',
+    stage_name: 'Linting',
+    stages: [
+        [
+            name: 'Make Lint',
+            type: 'script',
+            label: 'Makefile',
+            script: {
+                withMaven {
+                    sh 'make lint'
+                }
+            },
+            match_on_all_branches: true,
+        ]
+    ]
+)
