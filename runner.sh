0b01f6bda4c10664fdbc3f613a885f7a19882d8b
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-01-31 14:47:57 +0100

    Remove runner.sh

--- runner.sh
+++
@@ -1,7 +0,0 @@
-#
-# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License 2.0;
-# you may not use this file except in compliance with the Elastic License 2.0.
-#
-
-python3 fullsync.py & python3 fetch_index.py & python3 deindex.py & python3 sync_user_permissions.py
