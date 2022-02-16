b306ea6e5cffa2c36073e8636db256d39b7a70db
nirav-elastic <nirav.kelaiya@elastic.co>
2022-02-16 12:49:35 +0530

    Fixing the linting issue: removing trialing whitespace

--- ees_sharepoint/base_command.py
+++ ees_sharepoint/base_command.py
@@ -10,7 +10,7 @@ etc. This module provides convenience interface defining the shared
 objects and methods that will can be used by commands."""
 import logging
 
-# For Python>=3.8 cached_property should be imported from functools, 
+# For Python>=3.8 cached_property should be imported from functools,
 # and for the prior versions it should be imported from cached_property
 try:
     from functools import cached_property
